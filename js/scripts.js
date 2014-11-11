a29334234b69d826dc896677d04cfbd1ce3c3624
Jonathan Reinink <jonathan@reinink.ca>
2014-11-11 10:05:04 -0500

    Add "share this report" section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,5 @@
 $(function() {
 
-    // Update hash on change
-    $('.checklist__input').change(function () {
-
-        var rows = [];
-
-        $('.checklist__input:checked').each(function(index) {
-            rows.push($(this).val());
-        });
-
-        window.location.hash = rows.join(',');
-    });
-
     // Update checklist on load
     var rows = window.location.hash.substring(1).split(',');
 
@@ -22,4 +10,24 @@ $(function() {
             }
         });
     }
+
+    // Update hash on change
+    $('.checklist__input').change(function () {
+
+        var rows = [];
+
+        $('.checklist__input:checked').each(function(index) {
+            rows.push($(this).val());
+        });
+
+        window.location.hash = rows.join(',');
+
+        $('.share__input').val(window.location);
+
+    }).trigger('change');
+
+    // Select share link on input click
+    $('.share__input').click(function () {
+        $(this).select();
+    });
 });
